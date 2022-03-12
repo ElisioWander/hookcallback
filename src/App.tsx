7e93d899007111e48fe7ec60f818188e1ba13345
ElisioWander <elisio741@hotmail.com>
2022-03-12 19:41:50 -0300

    about-memo-useMemo-useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,51 @@
-import { useState } from "react";
+import { useState, useMemo, useCallback } from "react";
 import { Item } from "./components/Item";
 
 function App() {
   const [items, setItems] = useState<string[]>([])
+  const [wishList, setWishList] = useState<string[]>([])
+
+  const [newItem, setNewItem] = useState('')
 
   function addItemToList() {
     setItems([...items, `Item ${items.length}`])
   }
 
+  //todas as vezes que o item for alterado a função addItemToWishList vai ser
+  //chamada novamente. Isso vale para qualquer função que receber, nesse caso, Item como parâmetro
+  //para evitar esse comportamento vale usar o hook useCallbak.
+  //useCallback vai fazer com que uma função(ou variável) fique gravada em um espaço na memória
+  //para que, uma vez tendo a suas propriedades alteradas, a função não passe a ocupar um novo
+  //espaço na memória. Dessa forma a função não vai ser chamada quando não for necessário
+  const addItemToWishList = useCallback((item: string) => {
+    //state representa o valor anterior do wishList
+    //setWishList vai receber os valores antigos de wishList, espalha-los no array
+    //e enviar o novo valor recebido
+    setWishList(state => [...state, item])
+  }, [])
+
+  //os calculos somente irão ser refeitos caso o valor
+  //da variável que o useMemo está recebendo como dependência seja alterado.
+  const countItemWithOne = useMemo(() => {
+    console.log('test')
+
+    return items.filter(item => item.includes('1')).length
+
+  }, [items])
+
+
   return (
     <div>
+      <input type="text" value={newItem} onChange={e => setNewItem(e.target.value)} />
       <button onClick={addItemToList} >Add</button>
       <ul>
           {items.map(item => {
-            return <Item key={item} title={item} />
+            return <Item key={item} title={item} onAddItemToWishList={addItemToWishList} />
           })}
-        </ul>
+      </ul>
+      <div>
+          Contagem de 1: {countItemWithOne}
+      </div>
     </div>
   );
 }
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -2,10 +2,34 @@ import { memo } from 'react'
 
 interface ItemProps {
     title: string;
+    onAddItemToWishList: (item: string) => void;
 }
 
 function ItemComponent(props: ItemProps) {
-    return <li>{props.title}</li>
+    return (
+        <li>{props.title}
+            <button onClick={() => props.onAddItemToWishList(props.title)} >
+                Add to wish list
+            </button>
+        </li>
+    )
 }
+
+//memo vai impedir que o componente seja renderizado, ou seja, passar pelo processo
+//de: criação de uma árvore virtual de elementos DOM, comparar uma com a outra e fazer as alterações; 
+//caso o elemento não tenha tido suas propriedades alteradas.
+
+//O memo faz uma comparação raza, ou seja, ele compara as propriedades para ver se seus
+//valores foram ou não alterados. Caso a propriedade estiver sendo alterada em um nível mais profundo,
+//como em espaço de memória, o memo não vai conseguir impedir que o componente seja renderizado
+//novamente.
+
+//um exemplo é a propriedade passada acima chamada onAddItemToWishList.
+//Essa propriedade está recebendo uma função que tem como parâmetro uma variável que pode
+//ser alterada de acordo com que o usuário digite algo no input. Todas as vezes que essa 
+//propriedade é alterada a função vai ser chamada novamente, e então, ela vai ocupar um novo
+//espaço na memória. Dessa forma, mesmo que o memo esteja por volta desse component(ItemComponent),
+//o hook não vai conseguir detectar que não houve mudança nas propriedades(por conta da comparação raza),
+//e o componente será renderizado novamente.
 
 export const Item = memo(ItemComponent)
